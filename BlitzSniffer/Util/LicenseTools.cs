29c99017f0d366fc8b6714cfac3350bf04f9568a
OatmealDome <julian@oatmealdome.me>
2021-01-19 18:57:38 -0500

    LicenseTools: Add local license caching

--- BlitzSniffer/Util/LicenseTools.cs
+++ BlitzSniffer/Util/LicenseTools.cs
@@ -1,4 +1,4 @@
-﻿using BlitzCommon.Resources;
+using BlitzCommon.Resources;
 using BlitzSniffer.Config;
 using SKM.V3;
 using SKM.V3.Methods;
@@ -15,6 +15,11 @@ namespace BlitzSniffer.Util
     {
         public static readonly string CRYPTOLENS_PUBLIC_KEY = "<RSAKeyValue><Modulus>kXe0NP7Dco5g85KOziWQT+oK21VkKwp+4XeR6GOTf46u2F3UwdFK3UYA1wXxIobbWoCpvX+7Yq/gGlV03IEqjzfxePwMXKd31EIFT7fez/hKz29YRD6A9pIJwqnHfJo8Xfje/6vxj83nvlvLXLgLutJs4tKK+hM43EAKy2NEs3mF/qeu88tPX3MMkrqrN0N2/I2tPnUgiMjV/pZ02wWhZSFnsfxhpcmwUI0mTYPcYa8317oG2BoXtNiS7wpurHygZPPRpcqc/BJjR7117N3IY7GIBa7qsBhcyzjr86m+Wt2s65kt3A5vI9jAjQ7cTIPIhzvWJCoeVOwTdjJSpjZsxw==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
         private static readonly string CRYPTOLENS_AUTH_TOKEN = "WyIyNTg2NDgiLCJueitsRVVmQWFpVWIwVHM5RzdtTjJkNjkxekxHR0czb2ROU2phNEVyIl0=";
+
+        private static readonly string CACHED_LICENSE_FILE = "CachedLicense.bsrsrc";
+        private static readonly byte[] CACHED_LICENSE_FILE_KEY = new byte[] { 0xed, 0x3d, 0xdd, 0xdc, 0x41, 0x62, 0xb9, 0x0e, 0xdb, 0x49, 0x89, 0x42, 0xe6, 0x64, 0xfc, 0x96 };
+
+        private static readonly int MAX_OFFLINE_DAYS = 5;
 
         private static LicenseTools _Instance;
 
@@ -35,12 +40,22 @@ namespace BlitzSniffer.Util
 
         public bool LoadAndVerifyLicense()
         {
-            LocalLicense = GetLicenseByInet();
-            if (LocalLicense == null || !LocalLicense.HasValidSignature(CRYPTOLENS_PUBLIC_KEY).IsValid())
+            LocalLicense = GetLicenseByFile();
+            if (LocalLicense == null || !LocalLicense.HasValidSignature(CRYPTOLENS_PUBLIC_KEY, MAX_OFFLINE_DAYS).IsValid())
             {
-                LocalLicense = null;
+                LocalLicense = GetLicenseByInet();
+                if (LocalLicense == null || !LocalLicense.HasValidSignature(CRYPTOLENS_PUBLIC_KEY).IsValid())
+                {
+                    LocalLicense = null;
 
-                return false;
+                    return false;
+                }
+
+                // Save to file for caching
+                string licenseStr = LocalLicense.SaveAsString();
+                SnifferResource resource = new SnifferResource(Encoding.UTF8.GetBytes(licenseStr), CACHED_LICENSE_FILE_KEY);
+
+                File.WriteAllBytes(CACHED_LICENSE_FILE, resource.Serialize());
             }
 
             return true;
@@ -67,6 +82,26 @@ namespace BlitzSniffer.Util
             }
 
             return keyResult.LicenseKey;
+        }
+
+        private LicenseKey GetLicenseByFile()
+        {
+            if (!File.Exists(CACHED_LICENSE_FILE))
+            {
+                return null;
+            }
+
+            try
+            {
+                SnifferResource resource = SnifferResource.Deserialize(File.ReadAllBytes(CACHED_LICENSE_FILE), CACHED_LICENSE_FILE_KEY);
+                string licenseStr = Encoding.UTF8.GetString(resource.Data);
+
+                return new LicenseKey().LoadFromString(CRYPTOLENS_PUBLIC_KEY, licenseStr);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
     }
