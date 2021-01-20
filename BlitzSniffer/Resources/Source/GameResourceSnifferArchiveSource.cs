99d14794a3050ccae21fa85999f2051cf42e3625
OatmealDome <julian@oatmealdome.me>
2021-01-19 22:16:06 -0500

    GameResourceSnifferArchiveSource: Use bsrsrc in release builds

--- BlitzSniffer/Resources/Source/GameResourceSnifferArchiveSource.cs
+++ BlitzSniffer/Resources/Source/GameResourceSnifferArchiveSource.cs
@@ -1,18 +1,46 @@
-﻿using BlitzCommon.Resources.Source;
+﻿using BlitzCommon.Resources;
+using BlitzCommon.Resources.Source;
+using BlitzSniffer.Util;
 using Nintendo.Archive;
+using Syroot.NintenTools.Yaz0;
+using System;
 using System.IO;
 
 namespace BlitzSniffer.Resources.Source
 {
     class GameResourceSnifferArchiveSource : GameResourceSource
     {
-        private readonly static string ArchivePath = "GameData.sarc";
+#if !DEBUG
+        private readonly static string ArchivePath = "GameData.bsrsrc";
+#else
+        private readonly static string ArchivePath = "GameData.szs";
+#endif
 
         private Sarc SnifferArchive;
 
         private GameResourceSnifferArchiveSource()
         {
-            SnifferArchive = new Sarc(File.OpenRead(ArchivePath));
+            byte[] data;
+#if !DEBUG
+            string keyBase64 = LicenseTools.Instance.GetDataObjectString("gameDataKey");
+            byte[] key = Convert.FromBase64String(keyBase64);
+
+            SnifferResource resource = SnifferResource.Deserialize(File.ReadAllBytes(ArchivePath), key);
+
+            data = resource.Data;
+#else
+            data = File.ReadAllBytes(ArchivePath);
+#endif
+
+            using (MemoryStream resourceStream = new MemoryStream(data))
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                Yaz0Compression.Decompress(resourceStream, memoryStream);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                SnifferArchive = new Sarc(memoryStream);
+            }
         }
 
         public static void Initialize()
