27d9ea75b53e80d676f49767d93382106b762a5c
OatmealDome <julian@oatmealdome.me>
2021-06-26 23:21:10 -0400

    SnifferTextApplication: Add basic application with Console output

---
+++ BlitzSniffer/TextInterface/SnifferTextApplication.cs
@@ -0,0 +1,40 @@
+﻿using BlitzSniffer.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Terminal.Gui;
+
+namespace BlitzSniffer.TextInterface
+{
+    class SnifferTextApplication : Window
+    {
+        public SnifferTextApplication() : base("BlitzSniffer")
+        {
+            TabView tabView = new TabView()
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            // Console tab
+
+            TextView textView = new TextView()
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            tabView.AddTab(new TabView.Tab("Console", textView), true);
+
+            Console.SetOut(new TextViewWriter(textView));
+
+            //
+
+            this.Add(tabView);
+        }
+    }
+}
