420a6ffeec4f6e9b8daf1f77ee97d7156de14a27
OatmealDome <julian@oatmealdome.me>
2021-07-08 22:58:11 -0400

    PosixTimevalExtensions: Add extensions for addition and subtraction

---
+++ BlitzSniffer/Util/PosixTimevalExtensions.cs
@@ -0,0 +1,46 @@
+﻿using SharpPcap;
+
+namespace BlitzSniffer.Util
+{
+    public static class PosixTimevalExtensions
+    {
+        private const ulong ONE_SECOND_IN_MICROSECONDS = 1000000;
+
+        public static ulong ToTotalMicroseconds(this PosixTimeval timeval)
+        {
+            return (timeval.Seconds * ONE_SECOND_IN_MICROSECONDS) + timeval.MicroSeconds;
+        }
+
+        private static PosixTimeval Add(this PosixTimeval a, ulong microSeconds, bool minus = false)
+        {
+            ulong aMicroseconds = a.ToTotalMicroseconds();
+
+            ulong totalMicroseconds;
+            if (!minus)
+            {
+                totalMicroseconds = aMicroseconds + microSeconds;
+            }
+            else
+            {
+                totalMicroseconds = aMicroseconds - microSeconds;
+            }
+
+            ulong seconds = totalMicroseconds / ONE_SECOND_IN_MICROSECONDS;
+
+            return new PosixTimeval(seconds, totalMicroseconds - (seconds * ONE_SECOND_IN_MICROSECONDS));
+        }
+
+        // Operators are not supported as extensions, so this is the next best thing.
+        // Tracking: https://github.com/dotnet/csharplang/issues/192
+        public static PosixTimeval Add(this PosixTimeval a, PosixTimeval b)
+        {
+            return a.Add(b.ToTotalMicroseconds());
+        }
+
+        public static PosixTimeval Subtract(this PosixTimeval a, PosixTimeval b)
+        {
+            return a.Add(b.ToTotalMicroseconds(), true);
+        }
+
+    }
+}
